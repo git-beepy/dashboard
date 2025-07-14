80045b69210fa2d06f3da152e86e93252b65cda9
Beepy <gitbeepy@gmail.com>
2025-07-14 04:06:25 -0400

    feat: Fix vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -13,7 +14,7 @@ export default defineConfig({
     port: 3000,
     host: '0.0.0.0'
   },
-  base: './',
+  // base: './', // Remova ou comente esta linha
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
